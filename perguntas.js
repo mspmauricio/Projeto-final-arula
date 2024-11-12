bfa31832f8e9d902f2178e963cfe807594cb7738
mspmauricio <142758215+mspmauricio@users.noreply.github.com>
2024-11-12 17:32:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,48 @@
+criaCartao(
+    'Carros - Direção',
+    'Qual o tipo de direção automotiva mais atual?',
+    'Já passado pela direção mecânica, hidráulica e elétrica, a direção mais atual é a Eletro-hidráulica'
+)
+
+criaCartao(
+    'Carros - Funcionamento',
+    'Qual o sistema responsável por passar as marchas do carro?',
+    'É o sistema de câmbio'
+)
+
+criaCartao(
+    'Carros - Funcionamento',
+    'Qual o nome do disco que auxilia a frenagem do carro?',
+    'O disco que auxilia é o Disco de Freio, podendo também ser o Disco de Freio Ventilado'
+)
+
+criaCartao(
+    'Carros - Marca/História',
+    'Quais os primeiros veículos construídos pela marca Porsche?',
+    'Fusca em 1942; Logo em seguida foram os tanques durante a Segunda Guerra Mundial'
+)
+
+criaCartao(
+    'Carros - Funcionamento',
+    'Qual o principal líquido responsável pela lubrificação do motor?',
+    'O Óleo Lubrificante'
+)
+
+criaCartao(
+    'Carros - Funcionamento',
+    'Qual o nome da peça que conecta o pistão no virabrequim?',
+    'Biela'
+)
+
+criaCartao(
+    'Carros - História',
+    'Qual o tipo de motor a combustão mais atual?',
+    'V16, mas também pode ser acompanhado de um motor elétrico, se tornando um motor híbrido.'
+)
+
+criaCartao(
+    'Carros - História',
+    'Quando foi criado o primeiro carro?',
+    'Em 1886 por  Karl Benz, que criou o carro Benz Patent-Motorwagen',
+)
+
